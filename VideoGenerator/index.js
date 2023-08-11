fa62164cd3b8fbe60610a54e52d341d5be893643
Pratham <bhagatpratham101@gmail.com>
2023-08-11 22:14:14 +0530

    added fluent-ffmpeg support for genrating videos

--- VideoGenerator/index.js
+++ VideoGenerator/index.js
@@ -1,96 +1,188 @@
+// const axios = require("axios");
+// const fs = require("fs").promises;
+// const path = require("path");
+// const videoshow = require("videoshow");
+
+// module.exports = async function (context, req) {
+//   try {
+//     context.log(
+//       "HTTP trigger function for video generation processed a request."
+//     );
+
+//     const imageUrls = req.body.imageUrls;
+//     const timeInBetween = parseFloat(req.query.time_in_between) || 1.0;
+
+//     if (!imageUrls || !Array.isArray(imageUrls)) {
+//       context.res = {
+//         status: 400,
+//         body: "Invalid input. Provide an array of image URLs in the request body.",
+//       };
+//       return context.done();
+//     }
+
+//     const tempDir = path.join(
+//       context.executionContext.functionDirectory,
+//       "temp"
+//     );
+//     try {
+//       await fs.access(tempDir);
+//     } catch (error) {
+//       await fs.mkdir(tempDir);
+//     }
+
+//     const downloadedImages = [];
+//     for (const [index, imageUrl] of imageUrls.entries()) {
+//       try {
+//         const response = await axios.get(imageUrl, {
+//           responseType: "arraybuffer",
+//         });
+//         const imageName = `image_${index}.jpg`; // Assuming the images are JPG format
+//         const imagePath = path.join(tempDir, imageName);
+//         await fs.writeFile(imagePath, response.data);
+//         downloadedImages.push(imagePath);
+//       } catch (error) {
+//         context.log(`Error downloading ${imageUrl}: ${error.message}`);
+//       }
+//     }
+
+//     if (downloadedImages.length === 0) {
+//       context.res = {
+//         status: 500,
+//         body: "Failed to download any images.",
+//       };
+//       return context.done();
+//     }
+
+//     context.log("Generating video...");
+
+//     const videoOptions = {
+//       fps: 25,
+//       loop: 1, // seconds
+//       transition: true,
+//       transitionDuration: 1, // seconds
+//       videoBitrate: 1024,
+//       videoCodec: "libx264",
+//       size: "640x?",
+//       audioBitrate: "128k",
+//       audioChannels: 2,
+//       format: "mp4",
+//       pixelFormat: "yuv420p",
+//     };
+
+//     videoshow(downloadedImages, videoOptions)
+//       .save("video.mp4")
+//       .on("start", () => {
+//         context.log("Video generation started.");
+//       })
+//       .on("error", (err) => {
+//         context.log.error("Error generating video:", err.message);
+//         context.res = {
+//           status: 500,
+//           body: "Error generating video: " + err.message,
+//         };
+//       })
+//       .on("end", () => {
+//         context.log("Video successfully generated.");
+//         context.res = {
+//           status: 200,
+//           body: "Video generation and cleanup successful.",
+//         };
+//         context.done();
+//       });
+//     context.log("VIDEO GENERATED");
+//   } catch (error) {
+//     context.log.error("An error occurred:", error.message);
+//     context.res = {
+//       status: 500,
+//       body: "An error occurred: " + error.message,
+//     };
+//     context.done();
+//   }
+// };
+
 const axios = require("axios");
-const fs = require("fs");
+const fs = require("fs").promises;
 const path = require("path");
 const ffmpegPath = require("@ffmpeg-installer/ffmpeg").path;
 const ffmpeg = require("fluent-ffmpeg");
 ffmpeg.setFfmpegPath(ffmpegPath);
 
-// Azure Function entry point
 module.exports = async function (context, req) {
   try {
-    // Log that the function has been triggered
     context.log(
       "HTTP trigger function for video generation processed a request."
     );
 
-    // List of image URLs for the video
-    const imageUrls = [
-      "https://i.pinimg.com/736x/d4/9e/1e/d49e1e4bd32eb7281b50115ac938dcfb.jpg",
-      "https://e0.365dm.com/22/09/2048x1152/skysports-premier-league-promo_5897092.png",
-      "https://i.pinimg.com/736x/8d/87/a2/8d87a2aca6816f9fdd0cc57772f565f0.jpg",
-    ];
-    const timeInBetween = parseFloat(req.query.time_in_between) || 1.0; // Default to 1 second
+    const imageUrls = req.body.imageUrls;
+    const timeInBetween = parseFloat(req.query.time_in_between) || 1.0;
 
-    // Validate imageUrls input
     if (!imageUrls || !Array.isArray(imageUrls)) {
       context.res = {
         status: 400,
         body: "Invalid input. Provide an array of image URLs in the request body.",
       };
-      context.done();
       return;
     }
 
-    // Create a temporary directory for storing images
     const tempDir = path.join(
       context.executionContext.functionDirectory,
       "temp"
     );
-    if (!fs.existsSync(tempDir)) {
-      fs.mkdirSync(tempDir);
+    try {
+      await fs.mkdir(tempDir);
+    } catch (error) {
+      // Directory already exists, which is fine
     }
 
-    // Download and save images from imageUrls
-    const downloadedImages = [];
-    for (const imageUrl of imageUrls) {
-      try {
-        const response = await axios.get(imageUrl, {
-          responseType: "arraybuffer",
-        });
-        const imageName = path.basename(imageUrl);
-        const imagePath = path.join(tempDir, imageName);
-        fs.writeFileSync(imagePath, response.data);
-        downloadedImages.push(imagePath);
-      } catch (error) {
-        context.log(`Error downloading ${imageUrl}: ${error.message}`);
-      }
-    }
+    const downloadedImages = await Promise.all(
+      imageUrls.map(async (imageUrl, index) => {
+        try {
+          const response = await axios.get(imageUrl, {
+            responseType: "arraybuffer",
+          });
+          const imageName = `image_${index + 1}.jpg`;
+          const imagePath = path.join(tempDir, imageName);
+          await fs.writeFile(imagePath, response.data);
+          return imagePath;
+        } catch (error) {
+          context.log(`Error downloading ${imageUrl}: ${error.message}`);
+          return null;
+        }
+      })
+    );
 
-    // Check if any images were successfully downloaded
-    if (downloadedImages.length === 0) {
-      context.res = {
-        status: 500,
-        body: "Failed to download any images.",
-      };
-      context.done();
-      return;
-    }
-
-    // Generate video using downloaded images
     const outputVideoPath = path.join(tempDir, "output.mp4");
 
     const ffmpegCommand = ffmpeg();
-    for (const imagePath of downloadedImages) {
-      ffmpegCommand.input(imagePath);
+    for (let i = 0; i < downloadedImages.length + 1; i++) {
+      if (downloadedImages[i]) {
+        ffmpegCommand
+          .input(downloadedImages[i])
+          .inputOptions("-framerate", `${1 / timeInBetween}`);
+      }
     }
 
-    // Run the ffmpeg command to create the video
-    await new Promise((resolve, reject) => {
-      ffmpegCommand
-        .inputOptions(`-framerate ${1 / timeInBetween}`)
-        .output(outputVideoPath)
-        .on("end", () => {
-          context.log("Video generation successful.");
-          resolve();
-        })
-        .on("error", (err) => {
-          context.log.error("Error generating video:", err.message);
-          reject(err);
-        })
-        .run();
-    });
+    ffmpegCommand
+      .videoCodec("libx264")
+      .outputOptions("-pix_fmt", "yuv420p")
+      .output(outputVideoPath)
+      .on("end", () => {
+        context.log("Video generation successful.");
+        context.res = {
+          status: 200,
+          body: "Video generation and cleanup successful.",
+        };
+      })
+      .on("error", (err) => {
+        context.log.error("Error generating video:", err.message);
+        context.res = {
+          status: 500,
+          body: "Error generating video: " + err.message,
+        };
+        context.done();
+      })
+      .run();
   } catch (error) {
-    // Handle any errors that occur during the process
     context.log.error("An error occurred:", error.message);
     context.res = {
       status: 500,
